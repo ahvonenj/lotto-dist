1d48501f4f83553ef4eb9041b8e5426883297e8a
Jonah <jonah.ahvonen@gmail.com>
2015-05-02 02:55:33 +0300

    Add random number generation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require('path');
 var mu = require('mu2');
 var util = require('util');
+var Random = require("random-js");
+var random = new Random(Random.engines.mt19937().autoSeed());
 
 app.use('/style', express.static(__dirname + '/html/style'));
 app.use('/script', express.static(__dirname + '/html/script'));
@@ -10,11 +12,13 @@ app.use('/resource', express.static(__dirname + '/html/resource'));
 
 mu.root = __dirname + '/templates';
 
+var numbers = [];
+
 app.get('/', function (req, res) 
 {
     //res.sendFile(__dirname + '/index.html');
     
-    var stream = mu.compileAndRender('index.html', {name: "john"});
+    var stream = mu.compileAndRender('index.html', numbers);
     stream.pipe(res);
 });
 
@@ -22,3 +26,17 @@ var server = app.listen(3300, function ()
 {
     console.log('App initialized');
 });
+
+while(true)
+{
+    var value = random.integer(1, 39);
+    
+    if(typeof numbers[value] !== 'undefined')
+    {
+        numbers[value]++;
+    }
+    else
+    {
+        numbers.push(value);
+    }
+}
