e538cac6e4582ff7681db82e14ecae75e0a2a50e
Jonah <jonah.ahvonen@gmail.com>
2015-05-01 21:45:08 +0300

    Add mustache

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var mu = require('mu2');
+
+app.use('/style', express.static(__dirname + '/html/style'));
+app.use('/script', express.static(__dirname + '/html/script'));
+app.use('/resource', express.static(__dirname + '/html/resource'));
+
+mu.root = __dirname + '/templates';
 
 app.get('/', function (req, res) 
 {
-    res.send('Hello World!');
+    //res.sendFile(__dirname + '/index.html');
+    
+    
+    mu.compileAndRender('index.html', {name: "john"}).on('data', function(data) 
+    {
+        console.log(data.toString());
+    });
 });
 
 var server = app.listen(3300, function () 
