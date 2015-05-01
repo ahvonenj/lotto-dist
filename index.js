433dff23ee531f69ff61609310dbbee5d95c2df5
Jonah <jonah.ahvonen@gmail.com>
2015-05-01 23:24:05 +0300

    Remove example app log

--- index.js
+++ index.js
@@ -20,9 +20,5 @@ app.get('/', function (req, res)
 
 var server = app.listen(3300, function () 
 {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('App initialized');
 });
